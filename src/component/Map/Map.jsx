a53b03568ff1f72d0215a4eb324dae08cf1bf3c3
Christine Lo <christine.lo.hayes@gmail.com>
2021-12-18 23:02:30 -0500

    addgooglemap

--- src/component/Map/Map.jsx
+++ src/component/Map/Map.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
+import useStyles from './styles.js';
 const Map = function () {
-
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coordinates = {lat:0,lng:0};
   return (
-    <div>
-      map
-
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "process.env.REACT_APP_GOOGLE_MAP_API_KEY" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={ " " }
+        onChange={""}
+        onChildClick={''}
+       >
+</GoogleMapReact>
     </div>
 
   );
